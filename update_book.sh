3d090e9268b1dab63a32aac52fd6cc578bff0972
Bram van Dijk <bramvandijk88@gmail.com>
2025-10-14 16:11:41 +0200

    Dispatch book 2025-10-14

--- update_book.sh
+++ update_book.sh
@@ -3,19 +3,6 @@
 # cd Course_Website
 # cd Book
 
-cd _book 
-# Remove all HTML comments (<!-- ... -->) from HTML files in this directory
-
-# Find all .html files recursively
-find . -type f -name "*.html" | while read -r file; do
-    echo "Cleaning comments in: $file"
-    # Use Perl for robust multiline removal
-    perl -0777 -pe 's/<!--.*?-->//sg' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
-done
-cd ../
-
-echo "Succesfully removed all HTML comments."
-
 rm -rf docs
 cp -a _book docs
 date=$(date '+%Y-%m-%d')
